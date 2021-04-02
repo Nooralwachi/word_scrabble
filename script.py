132ebfb4ffbd339923714b5a63759510f1dab4b6
Nooralwachi <noor_alwachi@yahoo.com>
2021-04-02 16:31:30 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,32 +1,4 @@
-# Find the longest words in a given list of words that can be constructed from a given list of letters.
-#   Your solution should take as its first argument the name of a plain text file that contains one word per line.
-#   The remaining arguments define the list of legal letters. A letter may not appear in any single word more times than it appears in the list of letters.
-# Example input: :~$ script.py filename  a c t 
-# File contains:
 
-# at
-# act
-# art
-# cat
-# Tat
-# Tac
-
-# Script returns [act, cat]
-
-# prompt> scrabble.py WORD.LST w g d a s x z c y t e i o b
-# WORD.LST:
-# azotised
-# bawdiest
-# dystocia
-# geotaxis
-# iceboats
-# oxidates
-# oxyacids
-# sweatbox
-# tideways
-
-# Output:
-# ['azotised', 'bawdiest', 'dystocia', 'geotaxis', 'iceboats', 'oxidates', 'oxyacids', 'sweatbox', 'tideways']
 
 def word_scrabble(filename,letters):
 	with open(filename, 'r') as file:
